1f0e2b9f47b71f78a30489ef88c0093816cacd5d
leandrogripp <lggcontas@gmail.com>
2022-11-22 16:05:17 -0300

    Testes de integração

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -16,10 +16,26 @@ test('Testando cadastro de três perguntas', () => {
   modelo.cadastrar_pergunta('1 + 1 = ?');
   modelo.cadastrar_pergunta('2 + 2 = ?');
   modelo.cadastrar_pergunta('3 + 3 = ?');
-  const perguntas = modelo.listar_perguntas(); 
+  const perguntas = modelo.listar_perguntas();
   expect(perguntas.length).toBe(3);
   expect(perguntas[0].texto).toBe('1 + 1 = ?');
   expect(perguntas[1].texto).toBe('2 + 2 = ?');
   expect(perguntas[2].num_respostas).toBe(0);
-  expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
+  expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta - 1);
+});
+
+test('Testando cadastro de uma resposta', () => {
+  modelo.cadastrar_pergunta('1 + 1 = ?');
+  const { id_pergunta } = modelo.listar_perguntas()[0];
+  modelo.cadastrar_resposta(id_pergunta, '2');
+  const respostas = modelo.get_respostas(id_pergunta);
+  expect(respostas.length).toBe(1);
+  expect(respostas[0].texto).toBe('2');
+});
+
+test('Testando recuperar uma pergunta pelo id', () => {
+  modelo.cadastrar_pergunta('1 + 1 = ?');
+  const { id_pergunta } = modelo.listar_perguntas()[0];
+  const pergunta = modelo.get_pergunta(id_pergunta);
+  expect(pergunta.texto).toBe('1 + 1 = ?');
 });
